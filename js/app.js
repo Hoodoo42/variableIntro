131510bbe3525cc814772601e06849062def5647
Hoodoo42 <create@hoodooverse.ca>
2022-07-06 17:12:53 -0600

    learned the difference between = == and ===

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
-let firstName = "Colleen" ;
-let lastName = "Vereschagin" ;
-let yearBorn = 1988 ;
-let favouriteNumber = 14 ;
+let firstName = "Colleen";
+let lastName = "Vereschagin";
+let yearBorn = 1988;
+let favouriteNumber = 14;
 
-let userPoints = 42 ;
+let isMember = true;
+let userPoints = 93;
 
-if(userPoints >=75) {
-    console.log("CoNgRaTuLaTiOnS!");
-} else if(userPoints <=50 ){
-    console.log("Hey that's pretty okay!")
-    
-}else{
-console.log("Thats ok, your mom still thinks you're pretty great.")
+if (isMember === true && userPoints >= 90) {
+  console.log("Wow! You've set a new high score!");
+} else if (isMember === true && userPoints > 50) {
+  console.log("CoNgRaTuLaTiOnS!");
+} else if (isMember === true && userPoints <= 50) {
+  console.log("Thats ok, your mom still thinks you're pretty great.");
+} else if (isMember === false || userPoints < 10) {
+  console.log("You don't have to go home, but you can't stay here.");
+} else {
+  console.log("Game Over");
 }
