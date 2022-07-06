936ca48961b5e1b3cd5c2d30f54c8b64675644fd
Hoodoo42 <create@hoodooverse.ca>
2022-07-05 20:49:59 -0600

    added an else statement

--- js/app.js
+++ js/app.js
@@ -3,8 +3,10 @@ let lastName = "Vereschagin" ;
 let yearBorn = 1988 ;
 let favouriteNumber = 14 ;
 
-let userPoints = 82 ;
+let userPoints = 42 ;
 
 if(userPoints >=75) {
     console.log("CoNgRaTuLaTiOnS!");
+} else{
+    console.log("Thats ok, your mom still thinks you're pretty great.")
 }
