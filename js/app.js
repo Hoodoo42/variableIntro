ffddd5b8f7452d35750ae950277f669d9fb45be7
Hoodoo42 <create@hoodooverse.ca>
2022-07-05 20:39:36 -0600

    combined a variable and a conditional statement

--- js/app.js
+++ js/app.js
@@ -2,3 +2,9 @@ let firstName = "Colleen" ;
 let lastName = "Vereschagin" ;
 let yearBorn = 1988 ;
 let favouriteNumber = 14 ;
+
+let userPoints = 82 ;
+
+if(userPoints >=75) {
+    console.log("CoNgRaTuLaTiOnS!");
+}
